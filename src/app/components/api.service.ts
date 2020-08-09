2816e44e21ebf683bb768cdc57c24fc65d76c000
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-08-09 12:01:04 +0300

    created api service

---
+++ src/app/components/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  baseUrl = 'https://mbima.bluewaveinsurance.co:4435/api';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: localStorage.getItem('token')
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  login(endpoint: string, data: JSON) {
+    return this.http.post<any>(this.baseUrl + endpoint, data);
+  }
+
+  get(endpoint: string) {
+    return this.http.get<any>(this.baseUrl + endpoint, this.httpOptions);
+  }
+}
