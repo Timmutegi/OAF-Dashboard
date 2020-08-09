df8671137034bb58d4032d96060989fd4a18bf5e
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-08-09 12:01:46 +0300

    updated details component

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  ID: string;
+  claim: any;
+  isLoading = true;
+  pdfSrc: string;
 
-  constructor() { }
+  constructor(private api: ApiService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.ID = this.activatedRoute.snapshot.params.ID;
+    this.getClaim();
+  }
+
+  getClaim() {
+    this.api.get('/Claims/' + this.ID).subscribe(
+      res => {
+        console.log(res);
+        this.claim = res;
+        this.pdfSrc = res.file;
+      }
+    );
   }
 
 }
