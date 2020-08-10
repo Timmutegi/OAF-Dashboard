fb856f2f97a6752f2bfe266b09b8ed4321af6ff0
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-08-10 13:08:39 +0300

    Added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+import {MatTableModule} from '@angular/material/table';
+import { MatTableExporterModule } from 'mat-table-exporter';
+import {
+  MatPaginatorModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatProgressSpinnerModule,
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailsComponent } from './components/details/details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    DetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AngularFontAwesomeModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatTableExporterModule,
+    MatProgressSpinnerModule,
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
